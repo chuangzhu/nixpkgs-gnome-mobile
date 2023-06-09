9dbb724612f33c7fb8971ca73c5b3ebadde9ae38
Chuang Zhu <git@chuang.cz>
2023-06-09 22:23:44 +0800

    add update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p coreutils common-updater-scripts git
+
+tmpdir="$(mktemp -d)"
+git clone --bare --depth=1 --branch=mobile-shell https://gitlab.gnome.org/verdre/gnome-shell.git "$tmpdir"
+pushd "$tmpdir"
+new_version="unstable-$(git show -s --pretty='format:%cs')"
+commit_sha="$(git show -s --pretty='format:%H')"
+popd
+update-source-version "gnome.gnome-shell" --file=overlay.nix "$new_version" --rev="$commit_sha"
+
+tmpdir="$(mktemp -d)"
+git clone --bare --depth=1 --branch=mobile-shell https://gitlab.gnome.org/verdre/mutter.git "$tmpdir"
+pushd "$tmpdir"
+new_version="unstable-$(git show -s --pretty='format:%cs')"
+commit_sha="$(git show -s --pretty='format:%H')"
+popd
+update-source-version "gnome.mutter" --file=overlay.nix "$new_version" --rev="$commit_sha"
