fb0b2afd83eff9babdc5eba23af4f6d22e418d75
Chuang Zhu <git@chuang.cz>
2025-06-01 21:09:07 +0800

    [untested] 48.mobile.0

--- overlay.nix
+++ overlay.nix
@@ -8,21 +8,34 @@ let
     rev = "5f9768a2eac29c1ed56f1fbb449a77a3523683b6";
     hash = "sha256-gdgTnxzH8BeYQAsvv++Yq/8wHi7ISk2LTBfU8hk12NM=";
   };
+  gvdb = super.fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = "gvdb";
+    rev = "b54bc5da25127ef416858a3ad92e57159ff565b3";
+    hash = "sha256-c56yOepnKPEYFcU1B1TrDl8ydU0JU+z6R8siAQP4d2A=";
+  };
 in
 
 {
-  gnome-shell = (super.callPackage ./nixpkgs/gn/gnome-shell/package.nix { }).overrideAttrs (old: rec {
-    version = "46-mobile.1";
+  gnome-shell = super.gnome-shell.overrideAttrs (old: rec {
+    version = "48.mobile.0";
     src = super.fetchFromGitLab {
       domain = "gitlab.gnome.org";
       owner = "verdre";
       repo = "gnome-shell-mobile";
       rev = version;
-      hash = "sha256-NL1/mddfaL1rMidsbtV4kG2SlAZZNuR8KmqTmEE4IAM=";
+      hash = "sha256-Iu61qtK0j4OIWpuFjzx8v2G7H7jAbmSBpayuf2h5zUE=";
       fetchSubmodules = true;
     };
     postPatch = ''
-      patchShebangs src/data-to-c.pl
+      patchShebangs \
+        src/data-to-c.py \
+        meson/generate-app-list.py
+
+      # We can generate it ourselves.
+      rm -f man/gnome-shell.1
+
       ln -sf ${gvc} subprojects/gvc
     '';
     buildInputs = old.buildInputs ++ [
@@ -34,18 +47,33 @@ in
     '';
   });
 
-  mutter = (super.callPackage ./nixpkgs/mu/mutter/package.nix { mesa = super.libgbm; }).overrideAttrs (old: rec {
-    version = "46-mobile.1";
+  gnome-settings-daemon = super.gnome-settings-daemon.overrideAttrs (old: rec {
+    version = "48.mobile.0";
+    src = super.fetchFromGitLab {
+      domain = "gitlab.gnome.org";
+      owner = "verdre";
+      repo = "gnome-settings-daemon-mobile";
+      rev = version;
+      hash = "sha256-gLYcjlQ0IcItktRkMEP9k/thYX9sWFzm5P2KF4CS1u8=";
+    };
+    postPatch = old.postPatch + ''
+      rm -r subprojects/gvc
+      ln -sf ${gvc} subprojects/gvc
+    '';
+  });
+
+  mutter = super.mutter.overrideAttrs (old: rec {
+    version = "48.mobile.0";
     src = super.fetchFromGitLab {
       domain = "gitlab.gnome.org";
       owner = "verdre";
       repo = "mutter-mobile";
       rev = version;
-      hash = "sha256-Xmoq//Igaz1oVt2/aLV+9WjZzW1g6yLADqg97wD3Lug=";
+      hash = "sha256-Qv2a9siPMHJ2dFTpYqDkaHML0jQ89RDpgDu6z6j9Xrc=";
     };
-    buildInputs = old.buildInputs ++ [
-      super.mesa-gl-headers # EGL/eglmesaext.h: No such file or directory
-    ];
+    postPatch = old.postPatch + ''
+      ln -sf ${gvdb} subprojects/gvdb
+    '';
   });
 }
 
