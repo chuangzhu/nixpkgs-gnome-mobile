45bf1c0c3f925958bf1311ea387238c2be6fd7b7
Chuang Zhu <git@chuang.cz>
2026-02-24 06:56:57 +0800

    48.mobile.0 -> 49.mobile.0-unstable-2026-01-30
    I tried fixing 48.mobile.0 but I got tons of GJS errors. Let's bump to
    mobile-shell-devel-49 instead.
    
    Caveats: swipe-up-to-close-app gesture does not seem to work
    
    Closes #7 #9

--- overlay.nix
+++ overlay.nix
@@ -8,6 +8,13 @@ let
     rev = "5f9768a2eac29c1ed56f1fbb449a77a3523683b6";
     hash = "sha256-gdgTnxzH8BeYQAsvv++Yq/8wHi7ISk2LTBfU8hk12NM=";
   };
+  libshew = super.fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = "libshew";
+    rev = "ed782477cb5164320ae4f731d49bc5d475ab2a52";
+    hash = "sha256-auv5JsQUkytLLAAJzZlCDw4+v9/JfsCV1hKTa3Ku/Jg=";
+  };
   gvdb = super.fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "GNOME";
@@ -18,69 +25,73 @@ let
 in
 
 {
-  gnome-shell = super.gnome-shell.overrideAttrs (old: rec {
-    version = "48.mobile.0";
-    src = super.fetchFromGitLab {
-      domain = "gitlab.gnome.org";
-      owner = "verdre";
-      repo = "gnome-shell-mobile";
-      rev = version;
-      hash = "sha256-Iu61qtK0j4OIWpuFjzx8v2G7H7jAbmSBpayuf2h5zUE=";
-      fetchSubmodules = true;
-    };
-    postPatch = ''
-      patchShebangs \
-        src/data-to-c.py \
-        meson/generate-app-list.py
+  gnome-shell =
+    (super.gnome-shell.override {
+      gnome-settings-daemon = self.gnome-settings-daemon-mobile;
+      mutter = self.mutter;
+    }).overrideAttrs
+      (old: {
+        version = "49.mobile.0-unstable-2026-01-30";
+        src = super.fetchFromGitLab {
+          domain = "gitlab.gnome.org";
+          owner = "verdre";
+          repo = "gnome-shell-mobile";
+          rev = "f9e97c2f43827f22a1a3d7bbda8f8c7e88b450f9"; # mobile-shell-devel-49
+          hash = "sha256-XafMWhohuET+1S3At+I+wykqHaL7cM+bYYmTYt74hNs=";
+          fetchSubmodules = true;
+        };
+        prePatch = ''
+          cp -r ${libshew} subprojects/libshew
+          chmod -R u+w subprojects/libshew
+          cp -r ${gvc} subprojects/gvc
+          chmod -R u+w subprojects/gvc
+        '';
+        postPatch = ''
+          patchShebangs \
+            src/data-to-c.py \
+            build-aux/generate-app-list.py
 
-      # We can generate it ourselves.
-      rm -f man/gnome-shell.1
+          # We can generate it ourselves.
+          rm -f man/gnome-shell.1
+        '';
+        buildInputs = old.buildInputs ++ [
+          super.modemmanager # /org/gnome/shell/misc/modemManager.js
+          super.libgudev # /org/gnome/gjs/modules/esm/gi.js
+        ];
+        postFixup = old.postFixup + ''
+          wrapGApp $out/share/gnome-shell/org.gnome.Shell.SensorDaemon
+        '';
+      });
 
-      ln -sf ${gvc} subprojects/gvc
-    '';
-    buildInputs = old.buildInputs ++ [
-      super.modemmanager # /org/gnome/shell/misc/modemManager.js
-      super.libgudev # /org/gnome/gjs/modules/esm/gi.js
-    ];
-    postFixup =
-      old.postFixup
-      + ''
-        wrapGApp $out/share/gnome-shell/org.gnome.Shell.SensorDaemon
-      '';
-  });
-
-  gnome-settings-daemon-mobile = super.gnome-settings-daemon.overrideAttrs (old: rec {
-    version = "48.mobile.0";
+  gnome-settings-daemon-mobile = super.gnome-settings-daemon.overrideAttrs (old: {
+    version = "49.mobile.0-unstable-2026-01-30";
     src = super.fetchFromGitLab {
       domain = "gitlab.gnome.org";
       owner = "verdre";
       repo = "gnome-settings-daemon-mobile";
-      rev = version;
-      hash = "sha256-gLYcjlQ0IcItktRkMEP9k/thYX9sWFzm5P2KF4CS1u8=";
+      rev = "11b4c4a7812a9ad6cca4796b715e6ec8b7c55c3e"; # gnome-49-mobile
+      hash = "sha256-Lv79Vx7D7eAVC8alWA8V1aBOmhgXf5mtvZgpPiqx3dk=";
     };
-    postPatch =
-      old.postPatch
-      + ''
-        rm -r subprojects/gvc
-        ln -sf ${gvc} subprojects/gvc
-      '';
+    prePatch = ''
+      rm -r subprojects/gvc
+      cp -r ${gvc} subprojects/gvc
+      chmod -R u+w subprojects/gvc
+    '';
   });
 
   mutter =
     (super.mutter.override { gnome-settings-daemon = self.gnome-settings-daemon-mobile; }).overrideAttrs
-      (old: rec {
-        version = "48.mobile.0";
+      (old: {
+        version = "49.mobile.0-unstable-2026-01-30";
         src = super.fetchFromGitLab {
           domain = "gitlab.gnome.org";
           owner = "verdre";
           repo = "mutter-mobile";
-          rev = version;
-          hash = "sha256-Qv2a9siPMHJ2dFTpYqDkaHML0jQ89RDpgDu6z6j9Xrc=";
+          rev = "3d1ac0577cb13baa11e8fe6ee4b192d4b26c7a7a"; # mobile-shell-devel-49
+          hash = "sha256-qVwH3/HhxcgyGAbWMYI7t2tJLSNLT9ktJZrwlMonAes=";
         };
-        postPatch =
-          old.postPatch
-          + ''
-            ln -sf ${gvdb} subprojects/gvdb
-          '';
+        postPatch = old.postPatch + ''
+          ln -sf ${gvdb} subprojects/gvdb
+        '';
       });
 }
